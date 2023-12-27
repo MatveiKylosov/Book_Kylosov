c163eab70d0eb7c9d3209269d4e163e10600d4c6
MatveiKylosov <matveikyl@mail.ru>
2023-12-27 22:00:30 +0500

    Выгрузка/Загрузка данных

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Book_Kylosov.Classes;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +12,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +24,15 @@ namespace Book_Kylosov
     /// </summary>
     public partial class MainWindow : Window
     {
+#if true
+        List<Classes.Author> AllAuthors = new List<Classes.Author>();
+        List<Classes.Genre> AllGenres = new List<Classes.Genre>();
+        List<Classes.Book> AllBooks = new List<Classes.Book>();
+#else
         List<Classes.Author> AllAuthors = Classes.Author.AllAuthors();
         List<Classes.Genre> AllGenres = Classes.Genre.AllGenres();
         List<Classes.Book> AllBooks = Classes.Book.AllBook();
+#endif
 
         public MainWindow()
         {
@@ -99,5 +109,106 @@ namespace Book_Kylosov
         }
 
         private void SelectAuthor(object sender, SelectionChangedEventArgs e) => Search();
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Book| *.book";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string fio = $"";
+                string genre = $"";
+                string book = $"";
+
+                for (int i = 0; i < AllAuthors.Count; i++)
+                    fio += $"{AllAuthors[i].FIO}{(AllAuthors.Count - 1 == i ? "" : "|")}";
+
+                for (int i = 0; i < AllGenres.Count; i++)
+                    genre += $"{AllGenres[i].Name}{(AllGenres.Count - 1 == i ? "" : "|")}";
+
+                for (int i = 0; i < AllBooks.Count; i++)
+                {
+                    string aut = "";
+                    string genr = "";
+
+                    for(int f = 0; f < AllBooks[i].Authors.Count; f++)
+                        aut += $"{AllBooks[i].Authors[f].FIO}{(f == AllBooks[i].Authors.Count - 1 ? "" : ",")}";
+
+                    for (int f = 0; f < AllBooks[i].Genres.Count; f++)
+                        genr += $"{AllBooks[i].Genres[f].Name}{(f == AllBooks[i].Genres.Count- 1 ? "" : ",")}";
+
+                    book += $"{AllBooks[i].Name};{aut};{genr};{AllBooks[i].Year}{(AllBooks.Count - 1 == i ? "" : "|")}";
+                }
+
+                var fileStream = saveFileDialog.FileName;
+                StreamWriter streamWriter = new StreamWriter(fileStream);
+
+                streamWriter.WriteLine(fio);
+                streamWriter.WriteLine(genre);
+                streamWriter.WriteLine(book);
+
+                streamWriter.Close();
+            }
+        }
+
+        private void Import_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Book| *.book";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string booksStr;
+
+                using (StreamReader readde = new StreamReader(openFileDialog.FileName))
+                    booksStr = readde.ReadToEnd();
+
+                string[] strings = booksStr.Split('\n');
+
+
+                foreach (string x in strings[0].Split('|'))
+                    if (AllAuthors.Find(f => f.FIO == x) == null)
+                    {
+                        AllAuthors.Add(new Classes.Author(AllAuthors.Count() + 1, x));
+                    }
+
+
+                foreach (string x in strings[1].Split('|'))
+                    if (AllGenres.Find(f => f.Name == x) == null)
+                        AllGenres.Add(new Classes.Genre(AllGenres.Count() + 1, x));
+
+                foreach (string x in strings[2].Split('|'))
+                {
+                    if (AllBooks.Find(f => f.Name == x.Split(';')[0]) == null)
+                    {
+
+                        List<Author> aut = new List<Author>();
+                        List<Classes.Genre> genres = new List<Classes.Genre>();
+                    
+                        string[] fio = x.Split(';')[1].Split(',');
+                        string[] genre = x.Split(';')[2].Split(',');
+
+                        foreach (string f in fio)
+                        {
+                            Classes.Author a = AllAuthors.Find(item => item.FIO.Contains(f));
+                            if (a != null)
+                                aut.Add(a);
+                        }
+
+                        for (int i = 0; i < genre.Length; i++)
+                        {
+                            Classes.Genre ge = AllGenres.Find(item => item.Name.Contains(genre[i]));
+                            if (ge != null)
+                                genres.Add(ge);
+                        }
+
+                        AllBooks.Add(new Book(AllBooks.Count, x.Split(';')[0], genres, aut, 1337));
+                    }
+                }
+
+                CreateUI(AllBooks);
+            }
+        }
     }
 }
